fc64dd5ee7ac5949589d4e1c64891288837de245
scott <sflicker2@earthlink.net>
2021-06-18 18:44:41 -0400

    split main off from interpreter

--- pascal_interpreter.py
+++ pascal_interpreter.py
@@ -42,16 +42,16 @@
 # Restricted APIs
 # NOTE: eval and exec are disallowed in your solution.
 
-import codewars_test as Test
+#import codewars_test as Test
 
-import enum
-from pascal_tokenizer import Tokenizer
+#import enum
+#from pascal_tokenizer import Tokenizer
 from pascal_tokenizer import TokenType
 
-from pascal_parser import Parser
-from pascal_symbol import SymbolTableBuilder
+#from pascal_parser import Parser
+#from pascal_symbol import SymbolTableBuilder
 from pascal_ast import NodeVisitor
-from pascal_semantic_analyzer import SemanticAnalyzer
+#from pascal_semantic_analyzer import SemanticAnalyzer
 
 ########################
 ## Tree Printer
@@ -360,90 +360,95 @@ class Interpreter(NodeVisitor):
     def visit_WhileStatement(self, node):
         while(self.visit(node.expr)):
             self.visit(node.statement)
-
-# tests = [
-#     ["1 + 1", 2],
-#     ["8/16", 0.5],
-#     ["3 -(-1)", 4],
-#     ["2 + -2", 0],
-#     ["10- 2- -5", 13],
-#     ["(((10)))", 10],
-#     ["3 * 5", 15],
-#     ["-7 * -(6 / 3)", 14]
-# ]
 #
-# for test in tests:
-#     Test.assert_equals(calc(test[0]), test[1])
-
-# program = """
-# Program part10;
-# VAR
-#     number       : INTEGER;
-#     a, b, c, x   : INTEGER;
-#     y            : REAL;
-# BEGIN
-#     begin
-#         number := 2;
-#         a := number;
-#         b := 10 * a + 10 * NUMBER DIV 4;
-#         c := a - -b;
-#     EnD;
-#     X := 11;
-#     y := 20 / 7 + 3.14;
+# # tests = [
+# #     ["1 + 1", 2],
+# #     ["8/16", 0.5],
+# #     ["3 -(-1)", 4],
+# #     ["2 + -2", 0],
+# #     ["10- 2- -5", 13],
+# #     ["(((10)))", 10],
+# #     ["3 * 5", 15],
+# #     ["-7 * -(6 / 3)", 14]
+# # ]
+# #
+# # for test in tests:
+# #     Test.assert_equals(calc(test[0]), test[1])
 #
-# END.
-# """
-
-def run_program(program):
-
-    print("expression", program)
-    tokenizer = Tokenizer(program)
-    tokens = tokenizer.get_tokens()
-    print("tokens")
-    print(*tokens, sep='\n')
-
-    print("\nParsing")
-    parser = Parser(tokens)
-    tree = parser.parse()
-
-#    print("Parsed Tree")
-#    ast_printer = ASTPrinter(tree)
-#    ast_printer.print()
-
-    print("\nCreating Symbol Table")
-    symtab_builder = SymbolTableBuilder()
-    symtab_builder.visit(tree)
-    print('\nSymbol Table Contents')
-    print(symtab_builder.symtab)
-
-    analyzer = SemanticAnalyzer()
-    analyzer.visit(tree)
-
-    interpreter = Interpreter(tree)
-    print("\n\n------Interpreting Program")
-    result = interpreter.interpret()
-    print("------Finished Interpreting Program")
-    print(result)
-
-    print('')
-    print('-------Run-time GLOBAL_MEMORY contents:')
-    for k,v in sorted(interpreter.GLOBAL_MEMORY.items()):
-        print('{} = {}'.format(k,v))
-
-
-def main():
-    import sys
-#    text = open("test_files/part10.pas", 'r').read()
-#    text = open("test_files/simplest.pas", 'r').read()
-#    text = open("test_files/simplest2.pas", 'r').read()
-#    text = open("test_files/if.pas", 'r').read()
-#    text = open("test_files/part11.pas", 'r').read()
-    text = open("test_files/part12.pas", 'r').read()
-
-    run_program(text)
-
-
-
-
-if __name__ == '__main__':
-    main()
+# # program = """
+# # Program part10;
+# # VAR
+# #     number       : INTEGER;
+# #     a, b, c, x   : INTEGER;
+# #     y            : REAL;
+# # BEGIN
+# #     begin
+# #         number := 2;
+# #         a := number;
+# #         b := 10 * a + 10 * NUMBER DIV 4;
+# #         c := a - -b;
+# #     EnD;
+# #     X := 11;
+# #     y := 20 / 7 + 3.14;
+# #
+# # END.
+# # """
+#
+# def run_program(program):
+#
+#     print("expression", program)
+#     tokenizer = Tokenizer(program)
+#     tokens = tokenizer.get_tokens()
+#     print("tokens")
+#     print(*tokens, sep='\n')
+#
+#     print("\nParsing")
+#     parser = Parser(tokens)
+#     tree = parser.parse()
+#
+# #    print("Parsed Tree")
+# #    ast_printer = ASTPrinter(tree)
+# #    ast_printer.print()
+#
+#     print("\nCreating Symbol Table")
+#     symtab_builder = SymbolTableBuilder()
+#     symtab_builder.visit(tree)
+#     print('\nSymbol Table Contents')
+#     print(symtab_builder.symtab)
+#
+#     analyzer = SemanticAnalyzer()
+#     try:
+#         analyzer.visit(tree)
+#     except Exception as e:
+#         print(e)
+#
+#     print(analyzer.symbol_table)
+#
+#     interpreter = Interpreter(tree)
+#     print("\n\n------Interpreting Program")
+#     result = interpreter.interpret()
+#     print("------Finished Interpreting Program")
+#     print(result)
+#
+#     print('')
+#     print('-------Run-time GLOBAL_MEMORY contents:')
+#     for k,v in sorted(interpreter.GLOBAL_MEMORY.items()):
+#         print('{} = {}'.format(k,v))
+#
+#
+# def main():
+#     import sys
+# #    text = open("test_files/part10.pas", 'r').read()
+# #    text = open("test_files/simplest.pas", 'r').read()
+# #    text = open("test_files/simplest2.pas", 'r').read()
+# #    text = open("test_files/if.pas", 'r').read()
+# #    text = open("test_files/part11.pas", 'r').read()
+#     text = open("test_files/part12.pas", 'r').read()
+#
+#     run_program(text)
+#
+#
+#
+#
+# if __name__ == '__main__':
+#     main()
--- pascal_symbol.py
+++ pascal_symbol.py
@@ -43,20 +43,25 @@ class SymbolTable(object):
         self._init_builtins()
 
     def _init_builtins(self):
-        self.define(BuiltinTypeSymbol('INTEGER'))
-        self.define(BuiltinTypeSymbol('REAL'))
-        self.define(BuiltinTypeSymbol('STRING'))
+        self.insert(BuiltinTypeSymbol('INTEGER'))
+        self.insert(BuiltinTypeSymbol('REAL'))
+        self.insert(BuiltinTypeSymbol('STRING'))
 
     def __str__(self):
 
-        s = 'Symbols: {symbols}'.format(
-            symbols = [value for value in self._symbols.values()]
+        symtab_header = 'Symbol table contents'
+        lines = ['\n', symtab_header, '_' * len(symtab_header)]
+        lines.extend(
+            ('%7s: %r' % (key, value))
+            for key, value in self._symbols.items()
         )
+        lines.append('\n')
+        s = '\n'.join(lines)
         return s
 
     __repr__ = __str__
 
-    def define(self, symbol):
+    def insert(self, symbol):
         print('Define: %s' % symbol)
         self._symbols[symbol.name] = symbol
 
@@ -70,58 +75,58 @@ class SymbolTable(object):
 ### SymbolTableBuilder
 #############################
 
-class SymbolTableBuilder(NodeVisitor):
-    def __init__(self):
-        self.symtab = SymbolTable()
-
-    def visit_Program(self, node):
-        self.visit(node.block)
-
-    def visit_Block(self, node):
-        for declaration in node.declarations:
-            self.visit(declaration)
-        self.visit(node.compound_statement)
-
-    def visit_ProcedureDecl(self, node):
-        pass
-
-    def visit_Compound(self, node):
-        for child in node.children:
-            self.visit(child)
-
-    def visit_NoOp(self, node):
-        pass
-
-    def visit_BinOp(self, node):
-        self.visit(node.lhs)
-        self.visit(node.rhs)
-
-    def visit_Num(self, node):
-        pass
-
-    def visit_String(self, node):
-        pass
-
-    def visit_UnaryOp(self, node):
-        self.visit(node.operand)
-
-    def visit_VarDecl(self, node):
-        type_name = node.type_node.value
-        type_symbol = self.symtab.lookup(type_name)
-        var_name = node.var_node.value
-        var_symbol = VarSymbol(var_name, type_symbol)
-        self.symtab.define(var_symbol)
-
-    def visit_Assign(self, node):
-        var_name = node.lhs.value
-        var_symbol = self.symtab.lookup(var_name)
-        if var_symbol is None:
-            raise NameError(repr(var_name))
-        self.visit(node.rhs)
-
-    def visit_Var(self, node):
-        var_name = node.value
-        var_symbol = self.symtab.lookup(var_name)
-        if var_symbol is None:
-            raise NameError(repr(var_name))
+# class SymbolTableBuilder(NodeVisitor):
+#     def __init__(self):
+#         self.symtab = SymbolTable()
+#
+#     def visit_Program(self, node):
+#         self.visit(node.block)
+#
+#     def visit_Block(self, node):
+#         for declaration in node.declarations:
+#             self.visit(declaration)
+#         self.visit(node.compound_statement)
+#
+#     def visit_ProcedureDecl(self, node):
+#         pass
+#
+#     def visit_Compound(self, node):
+#         for child in node.children:
+#             self.visit(child)
+#
+#     def visit_NoOp(self, node):
+#         pass
+#
+#     def visit_BinOp(self, node):
+#         self.visit(node.lhs)
+#         self.visit(node.rhs)
+#
+#     def visit_Num(self, node):
+#         pass
+#
+#     def visit_String(self, node):
+#         pass
+#
+#     def visit_UnaryOp(self, node):
+#         self.visit(node.operand)
+#
+#     def visit_VarDecl(self, node):
+#         type_name = node.type_node.value
+#         type_symbol = self.symtab.lookup(type_name)
+#         var_name = node.var_node.value
+#         var_symbol = VarSymbol(var_name, type_symbol)
+#         self.symtab.define(var_symbol)
+#
+#     def visit_Assign(self, node):
+#         var_name = node.lhs.value
+#         var_symbol = self.symtab.lookup(var_name)
+#         if var_symbol is None:
+#             raise NameError(repr(var_name))
+#         self.visit(node.rhs)
+#
+#     def visit_Var(self, node):
+#         var_name = node.value
+#         var_symbol = self.symtab.lookup(var_name)
+#         if var_symbol is None:
+#             raise NameError(repr(var_name))
 
