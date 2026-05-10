e207f644aa8f85bd477fe1d98e7d5fa7a8737f11
Scott Flicker <sflicker2@earthlink.net>
2026-05-10 16:22:47 -0400

    adding example that uses formatted output

---
+++ examples/diag.pas
@@ -0,0 +1,11 @@
+{ This exercise is [Section 1.9 exer. 1]  from Problem solving and structured programming in pascal by Hoffan }
+program Diagonal;
+
+var
+    i : integer;
+begin
+    for i := 1 to 5 do
+    begin
+        writeln('*':i);
+    end;
+end.
